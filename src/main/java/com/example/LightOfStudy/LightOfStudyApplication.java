71321eca9fdea157a87e26190e95450370549e8d
greenbeam <oreshka@gmail.com>
2022-08-26 22:41:58 +0300

    My site

---
+++ src/main/java/com/example/LightOfStudy/LightOfStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.LightOfStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightOfStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightOfStudyApplication.class, args);
+	}
+
+}
